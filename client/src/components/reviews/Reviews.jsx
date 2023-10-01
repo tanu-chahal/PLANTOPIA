447b6b3e09be06099baae51ac1d3a0e289e6e657
Tanu Chahal <tanuchahal0004@gmail.com>
2023-10-01 16:50:46 +0530

    Home Component State Management Done

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -2,10 +2,15 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import StarIcon from "@mui/icons-material/Star";
 import React from "react";
+import { reviews } from "../../utils/data";
+import { useRecoilState } from "recoil";
+import { activeReview } from "../../recoilState.js";
 
 function Reviews() {
+  const [start, setStart] = useRecoilState(activeReview);
+
   return (
-    <Box sx={{ ml: 10, mb: 30, userSelect:'none' }}>
+    <Box sx={{ ml: 10, mb: 30, userSelect: "none" }}>
       <Box
         sx={{
           display: "flex",
@@ -23,30 +28,22 @@ function Reviews() {
           What customers say about PLANTOPIA?
         </Typography>
         <Box className="sliderDots" sx={{ display: "flex", gap: 1, mr: 10 }}>
-          <Box
-            sx={{
-              width: 40,
-              height: 6,
-              borderRadius: 3,
-              backgroundColor: "#c1dcdc",
-            }}
-          />
-          <Box
-            sx={{
-              width: 15,
-              height: 6,
-              borderRadius: 3,
-              backgroundColor: "#8f8f8f",
-            }}
-          />
-          <Box
-            sx={{
-              width: 15,
-              height: 6,
-              borderRadius: 3,
-              backgroundColor: "#8f8f8f",
-            }}
-          />
+          {reviews.map((r, i) => {
+            return (
+              <Box
+                sx={{
+                  width: start === i ? 40 : 15,
+                  height: 6,
+                  borderRadius: 3,
+                  backgroundColor: start === i ? "#c1dcdc" : "#8f8f8f",
+                }}
+                key={r.id}
+                onClick={() => {
+                  setStart(i);
+                }}
+              />
+            );
+          })}
         </Box>
       </Box>
 
@@ -54,185 +51,72 @@ function Reviews() {
         className="ReviewsSlider"
         sx={{ overflow: "hidden", display: "flex", gap: 5 }}
       >
-        <Box
-          sx={{
-            backgroundColor: "#c1dcdc",
-            width: 800,
-            p: 5,
-            borderRadius: 5,
-            display: "flex",
-            flexDirection: "column",
-            gap: 5,
-            height: 250,
-            flexShrink: 0,
-          }}
-        >
-          <Typography color="secondary" component="p" variant="h6">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
-            fugiat modi a, quasi enim libero suscipit ipsam omnis placeat quam.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-            dignissimos.
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "flex-start",
-            }}
-          >
-            <Box sx={{ display: "flex", gap: 3 }}>
-              <img
-                src="/img/reviewer-1.jpg"
-                alt=""
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  objectFit: "cover",
-                  border: "2px solid black",
-                  borderRadius: "50%",
+        {reviews.slice(start, start + 2).map((r) => {
+          return (
+            <Box
+              sx={{
+                backgroundColor: "#c1dcdc",
+                width: 800,
+                p: 5,
+                borderRadius: 5,
+                display: "flex",
+                flexDirection: "column",
+                gap: 5,
+                height: 250,
+                flexShrink: 0,
+              }}
+              key={r.id}
+            >
+              <Typography color="secondary" component="p" variant="h6">
+                {r.id} {r.desc}
+              </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "flex-start",
                 }}
-              />
-              <Box>
-                <Typography
-                  component="h6"
-                  variant="subtitle1"
-                >
-                  John Doe
-                </Typography>
-                <Typography color="secondary" component="p" variant="subtitle3">
-                  Youtuber
-                </Typography>
+              >
+                <Box sx={{ display: "flex", gap: 3 }}>
+                  <img
+                    src={r.img}
+                    alt=""
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      objectFit: "cover",
+                      border: "2px solid black",
+                      borderRadius: "50%",
+                    }}
+                  />
+                  <Box>
+                    <Typography component="h6" variant="subtitle1">
+                      {r.name}
+                    </Typography>
+                    <Typography
+                      color="secondary"
+                      component="p"
+                      variant="subtitle3"
+                    >
+                      {r.profession}
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                  <StarIcon />
+                  <Typography
+                    component="h6"
+                    variant="h6"
+                    sx={{ fontWeight: 700 }}
+                  >
+                    {" "}
+                    {r.rating}
+                  </Typography>
+                </Box>
               </Box>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-              <StarIcon />
-              <Typography component="h6" variant="h6" sx={{ fontWeight: 700 }}>
-                {" "}
-                4.5
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-
-        <Box
-          sx={{
-            backgroundColor: "#c1dcdc",
-            width: 800,
-            p: 5,
-            borderRadius: 5,
-            display: "flex",
-            flexDirection: "column",
-            gap: 5,
-            height: 250,
-            flexShrink: 0,
-          }}
-        >
-          <Typography color="secondary" component="p" variant="h6">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
-            fugiat modi a, quasi enim libero suscipit ipsam omnis placeat quam.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-            dignissimos.
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "flex-start",
-            }}
-          >
-            <Box sx={{ display: "flex", gap: 3 }}>
-              <img
-                src="/img/reviewer-2.jpeg"
-                alt=""
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  objectFit: "cover",
-                  border: "2px solid black",
-                  borderRadius: "50%",
-                }}
-              />
-              <Box>
-                <Typography
-                  component="h6"
-                  variant="subtitle1"
-                >
-                  John Doe
-                </Typography>
-                <Typography color="secondary" component="p" variant="subtitle3">
-                  Youtuber
-                </Typography>
-              </Box>
-            </Box>
-            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-              <StarIcon />
-              <Typography component="h6" variant="h6" sx={{ fontWeight: 800 }}>
-                {" "}
-                4.5
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-
-        <Box
-          sx={{
-            backgroundColor: "#c1dcdc",
-            width: 800,
-            p: 5,
-            borderRadius: 5,
-            display: "flex",
-            flexDirection: "column",
-            gap: 5,
-            height: 250,
-            flexShrink: 0,
-          }}
-        >
-          <Typography color="secondary" component="p" variant="h6">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
-            fugiat modi a, quasi enim libero suscipit ipsam omnis placeat quam.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-            dignissimos.
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "flex-start",
-            }}
-          >
-            <Box sx={{ display: "flex", gap: 3 }}>
-              <img
-                src="/img/reviewer-2.jpeg"
-                alt=""
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  objectFit: "cover",
-                  border: "2px solid black",
-                  borderRadius: "50%",
-                }}
-              />
-              <Box>
-                <Typography
-                  component="h6"
-                  variant="subtitle1"
-                >
-                  John Doe
-                </Typography>
-                <Typography color="secondary" component="p" variant="subtitle3">
-                  Youtuber
-                </Typography>
-              </Box>
-            </Box>
-            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-              <StarIcon />
-              <Typography component="h6" variant="h6" sx={{ fontWeight: 800 }}>
-                {" "}
-                4.5
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
+          );
+        })}
       </Box>
     </Box>
   );
--- client/src/recoilState.js
+++ client/src/recoilState.js
@@ -1,11 +1,16 @@
 import { atom, selector } from "recoil";
 
 export const menuState = atom({
-    key: 'menuState', 
-    default: false,
+  key: "menuState",
+  default: false,
 });
 
 export const activePage = atom({
-    key: 'activePage', 
-    default:'/',
+  key: "activePage",
+  default: "/",
+});
+
+export const activeReview = atom({
+  key: "activeReview",
+  default: 0,
 });
